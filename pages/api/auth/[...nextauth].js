eee219422bb304a09d42d53f0fd4d64066bbd185
belle <bellesee1212@gmail.com>
2024-03-11 10:50:23 -0400

    redirect url

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -26,7 +26,7 @@ export default NextAuth({
     async redirect({ url, baseUrl }) {
       console.log("url: " + url)
       console.log("baseUrl: " + baseUrl)
-      return 'https://site-git-hw.hackclub.dev'
+      return 'https://site-git-hw.hackclub.dev/hackers-wanted'
     },
     async signIn({ user }) {
       console.log(user)
